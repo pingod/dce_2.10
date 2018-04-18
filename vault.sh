461dd00c78e9824b34f916cb6857e9dbce23bb4f
Jun Liu <jlive@Mac-for-Juneau.local>
2018-04-19 00:57:34 +0800

    增加flannel支持

---
+++ vault.sh
@@ -0,0 +1,12 @@
+VAULT_ID='myVAULT@2018'
+echo $VAULT_ID > ~/.vault_pass.txt
+
+ANSIBLE_USER='root' # 远程操作用户名
+ANSIBLE_PASSWORD='root' # 远程操作用户密码，如果通过密钥认证可以删除该变量及下面对应的加密步骤
+DCE_USER='myuser' # dce认证用户
+DCE_PASSWORD='myuser' # dce认证用户密码
+
+ansible-vault encrypt_string --vault-id ~/.vault_pass.txt $ANSIBLE_USER --name 'vault_ansible_user' | tee dev/group_vars/vault
+ansible-vault encrypt_string --vault-id ~/.vault_pass.txt $ANSIBLE_PASSWORD --name 'vault_ansible_password' | tee -a dev/group_vars/vault
+ansible-vault encrypt_string --vault-id ~/.vault_pass.txt $DCE_USER --name 'vault_dce_user' | tee -a dev/group_vars/vault
+ansible-vault encrypt_string --vault-id ~/.vault_pass.txt $DCE_PASSWORD --name 'vault_dce_password' | tee -a dev/group_vars/vault
