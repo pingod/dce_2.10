746d6ccaf0a22165fa85d4839e7ef588759b1f42
Jun Liu <jlive@Mac-for-Juneau.local>
2018-09-03 18:13:50 +0800

    clear

--- vault.sh
+++ vault.sh
@@ -2,7 +2,7 @@ VAULT_ID='myVAULT@2018'
 echo $VAULT_ID > ~/.vault_pass.txt
 
 ANSIBLE_USER='root' # 远程操作用户名
-ANSIBLE_PASSWORD='dangerous' # 远程操作用户密码，如果通过密钥认证可以删除该变量及下面对应的加密步骤
+ANSIBLE_PASSWORD='root' # 远程操作用户密码，如果通过密钥认证可以删除该变量及下面对应的加密步骤
 DCE_USER='myuser' # dce认证用户
 DCE_PASSWORD='myuser' # dce认证用户密码
 
